6ae3ce3aa7cdaa5e294e50443d2bdd19a9970f0b
mborik128 <mborik@users.sourceforge.net>
2023-02-18 15:58:41 +0100

    cleanup script

---
+++ clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# demo builder (c) 2019-2023 mborik/SinDiKat
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+rm -f bank*
+
+pushd build
+rm -f stopgeno*
+popd
+
+subdirs=(
+	kernel
+	music
+)
+
+for D in "${subdirs[@]}"; do
+	pushd "${D}"
+	rm -f final.bin *.lst *.pak *.sna *.tap
+	popd
+done
