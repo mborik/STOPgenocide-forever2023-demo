bcc827b3b62393db32d0d6d9d8d5b4392950b4fa
mborik128 <mborik@users.sourceforge.net>
2023-02-24 12:48:43 +0100

    move sprite2code to utils

---
+++ grinder/kiteyes-compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+node ../_utils/sprite2code.js kiteyes1.bin 4EAB 4
+node ../_utils/sprite2code.js kiteyes2.bin 4EAB 4
--- grinder/kiteyes_compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-node sprite2code.js kiteyes1.bin 4EAB 4
-node sprite2code.js kiteyes2.bin 4EAB 4
