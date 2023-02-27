94b5652fa7332f18ba48d74571c8c44d07133097
mborik128 <mborik@users.sourceforge.net>
2023-02-27 19:23:27 +0100

    recycle scenario done

--- recycle/preparegfx.sh
+++ recycle/preparegfx.sh
@@ -8,20 +8,31 @@ function PAK() {
 
 pushd gfx
 
+justtop=(
+	11
+	12
+)
+
+for FN in "${justtop[@]}"; do
+	dd if=${FN}.scr of=${FN}.part bs=4096 count=1 iflag=skip_bytes status=none
+done
+
 topack=(
 	01.scr
 	02.scr
 	03.scr
 	04.scr
-	05.scr
-	06.scr
-	11.scr
-	12.scr
+	11.part
+	12.part
 	13.scr
 )
 
 for FN in "${topack[@]}"; do
 	PAK ${FN} ${FN%.*}.pak
+
+	if [ "${FN##*.}" = "part" ]; then
+		rm ${FN}
+	fi
 done
 
 popd
