f89ddeb5759609cecc0254bdf65655222ccc81c8
mborik128 <mborik@users.sourceforge.net>
2023-03-16 10:30:35 +0100

    completelly reworked recycle bin

--- recyshow/preparegfx.sh
+++ recyshow/preparegfx.sh
@@ -8,31 +8,12 @@ function PAK() {
 
 pushd gfx
 
-justtop=(
-	11
-	12
-)
+PAK 01.scr 01.pak
 
-for FN in "${justtop[@]}"; do
-	head -c 4096 ${FN}.scr > ${FN}.part
-done
-
-topack=(
-	01.scr
-	02.scr
-	03.scr
-	04.scr
-	11.part
-	12.part
-	13.scr
-)
-
-for FN in "${topack[@]}"; do
-	PAK ${FN} ${FN%.*}.pak
-
-	if [ "${FN##*.}" = "part" ]; then
-		rm ${FN}
-	fi
-done
+tail -c +6145 04.scr > 04.tmp
+head -c 352 04.tmp > 04.attrs
+rm -f 04.tmp
+PAK 04.attrs 04.attrs.pak
+rm -f 04.attrs
 
 popd
